362787a382ab5a6f8a1a35676cb8fe561bd08cca
Turik <turalsuleymanov2006@gmail.com>
2024-12-20 17:29:58 +0800

    Контрольная работа по массивам

---
+++ kontrol/ex_1(4zadanie).cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int SIZE;
+
+    cout << "Введите размер массива: ";
+    cin >> SIZE;
+
+    if (SIZE <= 0) {
+        cout << "Размер массива должен быть больше 0!" << endl;
+        return 1; 
+    }
+
+    int* arr = new int[SIZE];
+
+    cout << "Введите элементы массива: ";
+    for (int i = 0; i < SIZE; ++i) {
+        cin >> arr[i];
+    }
+
+    int max_len = 0;  
+    int now_len = 0;  
+
+    for (int i = 0; i < SIZE; ++i) {
+        if (arr[i] == 0) {
+            now_len++; 
+        }
+        else {
+            if (now_len > max_len) {
+                max_len = now_len; 
+            }
+            now_len = 0; 
+        }
+    }
+
+    if (now_len > max_len) {
+        max_len = now_len;
+    }
+
+    cout << "Длина самой длинной последовательности нулей: " << max_len << endl;
+
+    delete[] arr;
+
+    return 0;
+}
---
+++ kontrol/ex_2(8zadanie).cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+ 
+    cout << "Введите длину массивов: ";
+    cin >> n;
+
+    int X[n], Y[n];
+
+
+    cout << "Введите элементы массива X: ";
+    for (int i = 0; i < n; i++) {
+        cin >> X[i];
+    }
+
+   
+    cout << "Введите элементы массива Y: ";
+    for (int i = 0; i < n; i++) {
+        cin >> Y[i];
+    }
+
+ 
+    bool match = true;
+    for (int i = 0; i < n; i++) {
+        if (X[i] != Y[i]) {
+            match = false;
+            break;
+        }
+    }
+
+   
+    if (match) {
+        cout << "ДА" << endl;
+    }
+    else {
+        cout << "НЕТ" << endl;
+    }
+
+    return 0;
+}
