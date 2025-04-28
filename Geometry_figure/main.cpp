f4568ad121ea8b8211f2b49e0f755fc39d037d14
Turik <turalsuleymanov2006@gmail.com>
2025-04-28 12:54:32 +0800

    map_count

--- Geometry_figure/main.cpp
+++ Geometry_figure/main.cpp
@@ -1,42 +1,36 @@
 ﻿#include "figure.h"
 #include <iostream>
 #include <vector>
+#include <map>
 
 int main() {
     setlocale(LC_ALL, "ru");
 
-    int circle_count = 0, ellipse_count = 0, triangle_count = 0;
-    int rectangle_count = 0, polygon_count = 0, scribble_count = 0;
+    std::map<std::string, int> figure_count;
+    std::map<std::string, double> area_sum;
+    std::map<std::string, double> perimeter_sum;
 
-    double circle_area_sum = 0, ellipse_area_sum = 0, triangle_area_sum = 0;
-    double rectangle_area_sum = 0, polygon_area_sum = 0;
-
-    double circle_perimeter_sum = 0, ellipse_perimeter_sum = 0, triangle_perimeter_sum = 0;
-    double rectangle_perimeter_sum = 0, polygon_perimeter_sum = 0;
+    int scribble_count = 0;
 
     std::vector<GeometricFigure*> figures;
- 
-    // Создаем несколько кругов
+
+    // Создаем несколько фигур
     figures.push_back(new Circle(Point(0, 0), 10));
     figures.push_back(new Circle(Point(5, 5), 7));
     figures.push_back(new Circle(Point(-3, 4), 12));
 
-    // Создаем несколько эллипсов
     figures.push_back(new Ellipse(Point(1, 1), 15, 5));
     figures.push_back(new Ellipse(Point(2, -2), 8, 3));
     figures.push_back(new Ellipse(Point(0, 5), 10, 4));
 
-    // Создаем несколько треугольников
     figures.push_back(new Triangle({ Point(0, 0), Point(0, 3), Point(4, 0) }));
     figures.push_back(new Triangle({ Point(1, 1), Point(4, 5), Point(7, 2) }));
     figures.push_back(new Triangle({ Point(-2, -2), Point(0, 3), Point(5, 0) }));
 
-    // Создаем несколько прямоугольников
     figures.push_back(new Rectangle({ Point(0, 0), Point(0, 3), Point(5, 3), Point(5, 0) }));
     figures.push_back(new Rectangle({ Point(2, 2), Point(2, 7), Point(6, 7), Point(6, 2) }));
     figures.push_back(new Rectangle({ Point(-5, -5), Point(-5, 0), Point(0, 0), Point(0, -5) }));
 
-    // Создаем многоугольник
     Polygon* poly1 = new Polygon("granitsy-uchastka2.txt");
     if (poly1->vertex_count() >= 3) figures.push_back(poly1);
     else delete poly1;
@@ -50,56 +44,30 @@ int main() {
         std::cout << "Площадь: " << area
             << ", Периметр: " << perimeter << "\n";
 
-        if (dynamic_cast<Circle*>(figures[i])) {
-            circle_count++;
-            circle_area_sum += area;
-            circle_perimeter_sum += perimeter;
-        }
-        else if (dynamic_cast<Ellipse*>(figures[i])) {
-            ellipse_count++;
-            ellipse_area_sum += area;
-            ellipse_perimeter_sum += perimeter;
-        }
-        else if (dynamic_cast<Triangle*>(figures[i])) {
-            triangle_count++;
-            triangle_area_sum += area;
-            triangle_perimeter_sum += perimeter;
-        }
-        else if (dynamic_cast<Rectangle*>(figures[i])) {
-            rectangle_count++;
-            rectangle_area_sum += area;
-            rectangle_perimeter_sum += perimeter;
-        }
-        else if (Polygon* p = dynamic_cast<Polygon*>(figures[i])) {
-            polygon_count++;
-            polygon_area_sum += area;
-            polygon_perimeter_sum += perimeter;
+        std::string type_name;
 
-            if (p->vertex_count() > 10)
-                scribble_count++;
+        if (dynamic_cast<Circle*>(figures[i])) type_name = "Круг";
+        else if (dynamic_cast<Ellipse*>(figures[i])) type_name = "Эллипс";
+        else if (dynamic_cast<Triangle*>(figures[i])) type_name = "Треугольник";
+        else if (dynamic_cast<Rectangle*>(figures[i])) type_name = "Прямоугольник";
+        else if (Polygon* p = dynamic_cast<Polygon*>(figures[i])) {
+            type_name = "Многоугольник";
+            if (p->vertex_count() > 10) scribble_count++;
         }
+
+        figure_count[type_name]++;
+        area_sum[type_name] += area;
+        perimeter_sum[type_name] += perimeter;
     }
 
+    // Вывод итогов
     std::cout << "\nИТОГИ:\n";
-    std::cout << "Кругов: " << circle_count
-        << ", Суммарная площадь: " << circle_area_sum
-        << ", Суммарный периметр: " << circle_perimeter_sum << "\n";
-
-    std::cout << "Эллипсов: " << ellipse_count
-        << ", Суммарная площадь: " << ellipse_area_sum
-        << ", Суммарный периметр: " << ellipse_perimeter_sum << "\n";
-
-    std::cout << "Треугольников: " << triangle_count
-        << ", Суммарная площадь: " << triangle_area_sum
-        << ", Суммарный периметр: " << triangle_perimeter_sum << "\n";
-
-    std::cout << "Прямоугольников: " << rectangle_count
-        << ", Суммарная площадь: " << rectangle_area_sum
-        << ", Суммарный периметр: " << rectangle_perimeter_sum << "\n";
-
-    std::cout << "Многоугольников: " << polygon_count
-        << ", Суммарная площадь: " << polygon_area_sum
-        << ", Суммарный периметр: " << polygon_perimeter_sum << "\n";
+    for (const auto& pair : figure_count) {
+        const std::string& name = pair.first;
+        std::cout << name << "ов: " << pair.second
+            << ", Суммарная площадь: " << area_sum[name]
+            << ", Суммарный периметр: " << perimeter_sum[name] << "\n";
+    }
 
     std::cout << "Многоугольников-каракули (больше 10 вершин): " << scribble_count << "\n";
 
